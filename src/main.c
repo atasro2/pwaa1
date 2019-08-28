3e4ff6a9ac87cb7e8fd8c4c7e010a391d0589882
Missingmew <nintendo6496@googlemail.com>
2019-08-28 19:29:42 +0200

    struct changes

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include "gba/gba.h"
 
 struct Struct3003730 {
-	u32 unk1;
-	u8 unk2;
+	u8 filler[4];
+	u8 unk4;
 };
 
 extern struct Struct3003730 gUnknown_03003730;
@@ -10,10 +10,10 @@ extern struct Struct3003730 gUnknown_03003730;
 void sub_080001EC() {
     u16 *input;
     u16 keys;
-    if(!gUnknown_03003730.unk2) {
+    if(!gUnknown_03003730.unk4) {
         input = (u16 *)0x04000130;
         keys = (0x3FF ^ *input);
         if (A_BUTTON & keys)
-            gUnknown_03003730.unk2 = 0xe;
+            gUnknown_03003730.unk4 = 0xe;
     }
 }
