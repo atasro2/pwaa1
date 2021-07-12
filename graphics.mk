55d2dbc436faac2bef978a319104dee4d8cfc57b
PhoenixBound <phoenixbound.github@gmail.com>
2021-07-12 00:08:26 -0500

    Dump the rest of the graphics

--- graphics.mk
+++ graphics.mk
@@ -733,5 +733,23 @@ graphics/title_screen_demo.8bpp.lz: graphics/title_screen_demo.8bpp
 	$(GBAGFX) $< $@ -search 8
 graphics/ui/trial/unused_testimony_characters.4bpp: graphics/ui/trial/unused_testimony_characters.png
 	$(GBAGFX) $< $@ -mwidth 2 -mheight 2
+graphics/ui/trial/press_present_buttons.4bpp: graphics/ui/trial/press_present_buttons.png
+	$(GBAGFX) $< $@ -mwidth 4 -mheight 2
+graphics/ui/court_record/profiles_text.4bpp: graphics/ui/court_record/profiles_text.png
+	$(GBAGFX) $< $@ -mwidth 4 -mheight 2
+graphics/ui/court_record/evidence_text.4bpp: graphics/ui/court_record/evidence_text.png
+	$(GBAGFX) $< $@ -mwidth 4 -mheight 2
+graphics/ui/trial/game_over_text.4bpp: graphics/ui/trial/game_over_text.png
+	$(GBAGFX) $< $@ -mwidth 8 -mheight 8
+graphics/ui/new_game_continue.4bpp: graphics/ui/new_game_continue.png
+	$(GBAGFX) $< $@ -mwidth 4 -mheight 2
+graphics/episode_select_options.4bpp: graphics/episode_select_options.png
+	$(GBAGFX) $< $@ -mwidth 8 -mheight 4
+graphics/episode_select_options.4bpp.lz: graphics/episode_select_options.4bpp
+	$(GBAGFX) $< $@ -search 1
+graphics/from_save_or_beginning_options.4bpp: graphics/from_save_or_beginning_options.png
+	$(GBAGFX) $< $@ -mwidth 8 -mheight 4
+graphics/speedlines_first_and_last_columns.4bpp: graphics/speedlines_first_and_last_columns.png
+	$(GBAGFX) $< $@ -mwidth 1 -mheight 20
 graphics/charset.4bpp: graphics/charset.png
 	$(GBAGFX) $< $@ -mwidth 2 -mheight 2
